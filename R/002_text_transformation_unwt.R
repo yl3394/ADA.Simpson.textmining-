e3d0608b2d70f9011401fd904bf8f19036193200
Yanjin Li (YJ) <yl3394@columbia.edu>
2016-10-10 11:56:35 -0400

    YJ

--- R/002_text_transformation_unwt.R
+++ R/002_text_transformation_unwt.R
@@ -100,44 +100,62 @@ NGramDataframe <- function(doc, n, sparse) {
   return(return.list)
 }
 
-
-
-
-
-
 #=================================================================================================================================================
-# STEP 6. Text Tansformation (Weighted)
+# STEP 4. N-gram Text Tansformation (Unweighted, N > 1)
 #=================================================================================================================================================
-# 6.1 Weighted DTM Generating 
+# N-gram Generator 
+NGramDataframe <- function(corpus, n, sparsity) {
+  # DESCRITPION: Return n-gram dataframes 
+  # RETURN VALUES: dataframe
+  return.list <- list()
+  NGramTokenizer <- function(x) 
+    unlist(lapply(ngrams(words(x), n), paste, collapse = " "), use.names = FALSE)
+  
+  ngram.dtm <- DocumentTermMatrix(corpus, control = list(tokenize = NGramTokenizer))
+  ngram.dtm.nonsparse <- removeSparseTerms(ngram.dtm, sparsity)    
+  ngram.dtm.df <- cbind(spoken_words = dat.scripts$spoken_words,
+                        spoken_words_clean = dat.scripts$spoken_words_clean,
+                        episode_id = dat.scripts$episode_id, # Details join by dat.episodes 
+                        speaking_line = dat.scripts$speaking_line,
+                        character_id = dat.scripts$character_id, # Details join by dat.character 
+                        location_id = dat.scripts$location_id, # Details join by dat.locations 
+                        as.data.frame(as.matrix(ngram.dtm.nonsparse))) 
+  return.list[["dtm"]] <- ngram.dtm
+  return.list[["dtm.df"]] <- ngram.dtm.df
+  return(return.list)
+}
+
+# 4.1 2-gram DTM Table 
 # ----------------------------
-myDTM.wt <- DocumentTermMatrix(myCorpus, control = list(weighting = weightTfIdf))
-myDTM.wt
-dim(myDTM.wt) 
-inspect(myDTM.wt[1:100, 1:100])
+gram2 <- NGramDataframe(myCorpus, 2, 0.9999) 
+myDTM.2 <- gram2$dtm
+dat.myDTM.2 <- gram2$dtm.df
 
-myDTM.wt.nosparse <- removeSparseTerms(myDTM.wt, 0.995) # dim = 127 
-dim(myDTM.wt.nosparse) 
+# Get Frequency Table 
+dat.DTMfrq.2 <- dat.myDTM.2 %>% 
+  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
+         -speaking_line, -character_id, -location_id) %>%
+  filter(frequency != 0) %>%
+  group_by(word) %>%    
+  summarise(num = n()) %>%           
+  arrange(desc(num)) 
 
-####### QUESTIONS: WHY DO WE NEED THE WEIGHTED IF-IDF? 
+write.csv(dat.DTMfrq.2, "exported_data/DTM_frq_2gram.csv")
 
+# 4.2 3-gram DTM Tables 
+# ----------------------------
+gram3 <- NGramDataframe(myCorpus, 3, 0.9999) 
+myDTM.3 <- gram3$dtm
+dat.myDTM.3 <- gram3$dtm.df
 
+# Get Frequency Table 
+dat.DTMfrq.3 <- dat.myDTM.3 %>% 
+  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
+         -speaking_line, -character_id, -location_id) %>%
+  filter(frequency != 0) %>%
+  group_by(word) %>%    
+  summarise(num = n()) %>%           
+  arrange(desc(num)) 
 
+write.csv(dat.DTMfrq.3, "exported_data/DTM_frq_3gram.csv")
 
-
-
-
-
-
-
-
-#=================================================================================================================================================
-# STEP 99. Test Values
-#=================================================================================================================================================
-# 99.1 Test on the DTM generator
-mtDTM.test <- DocumentTermMatrix(myCorpus[1:100])
-mtDTM.test
-dim(mtDTM.test)
-inspect(mtDTM.test)
-mtDTM.test.nosparse <- removeSparseTerms(mtDTM.test, 0.98)
-inspect(mtDTM.test.nosparse)
-dim(mtDTM.test.nosparse)
