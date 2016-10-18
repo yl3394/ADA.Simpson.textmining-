7acc7c3aa0a676c165c49ee595c4a1d814e4ffe9
Xuechun (Alexa) Sun <xuechunsun@gmail.com>
2016-10-17 23:27:14 -0400

    location_basic_analysis

--- R/008_location_basic_analysis.R
+++ R/008_location_basic_analysis.R
@@ -49,70 +49,52 @@ wordcloud(dat.DTMfrq.loct.SNPP$word, dat.DTMfrq.loct.SNPP$num, min.freq = 40, co
 # 1~10 (including 10) season: episode_id 1~226 
 # 1st Simpson Home;  location id = 5
 dat.loct.SH.episode <- dat.myDTM[which(dat.myDTM$location_id == 5),] %>%
-  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
-         -speaking_line, -character_id, -location_id) %>%
-  filter(frequency != 0) %>%
   group_by(episode_id) %>%
   summarise(num = n()) %>%
   arrange(episode_id)
 
 # 2nd Springfield Elementary School;  location id = 3
 dat.loct.SES.episode <- dat.myDTM[which(dat.myDTM$location_id == 3),] %>%
-  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
-         -speaking_line, -character_id, -location_id) %>%
-  filter(frequency != 0) %>%
   group_by(episode_id) %>%
   summarise(num = n()) %>%
   arrange(episode_id)
 
 # 3rd Moe's Tavern;  location id = 15
 dat.loct.MT.episode <- dat.myDTM[which(dat.myDTM$location_id == 15),] %>%
-  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
-         -speaking_line, -character_id, -location_id) %>%
-  filter(frequency != 0) %>%
   group_by(episode_id) %>%
   summarise(num = n()) %>%
   arrange(episode_id)
 
 # 4th Springfield Nuclear Power Plant; location id = 10
 dat.loct.SNPP.episode <- dat.myDTM[which(dat.myDTM$location_id == 10),] %>%
-  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
-         -speaking_line, -character_id, -location_id) %>%
-  filter(frequency != 0) %>%
   group_by(episode_id) %>%
   summarise(num = n()) %>%
   arrange(episode_id)
 
 # 5th Simpson Living Room; location id = 25
 dat.loct.SLR.episode <- dat.myDTM[which(dat.myDTM$location_id == 25),] %>%
-  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
-         -speaking_line, -character_id, -location_id) %>%
-  filter(frequency != 0) %>%
   group_by(episode_id) %>%
   summarise(num = n()) %>%
   arrange(episode_id)
 
 # Whole line number 
 dat.loct.episode <- dat.myDTM%>%
-  gather(word, frequency, -spoken_words, -spoken_words_clean, -episode_id,
-         -speaking_line, -character_id, -location_id) %>%
-  filter(frequency != 0) %>%
   group_by(episode_id) %>%
   summarise(num = n()) %>%
   arrange(episode_id)
 
 dat.location.episode.before <- rbind(sum(dat.loct.SH.episode[which(dat.loct.SH.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
-                              sum(dat.loct.SES.episode[which(dat.loct.SES.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
-                              sum(dat.loct.MT.episode[which(dat.loct.MT.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
-                              sum(dat.loct.SNPP.episode[which(dat.loct.SNPP.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
-                              sum(dat.loct.SLR.episode[which(dat.loct.SLR.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2])
-                              )
+                                     sum(dat.loct.SES.episode[which(dat.loct.SES.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
+                                     sum(dat.loct.MT.episode[which(dat.loct.MT.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
+                                     sum(dat.loct.SNPP.episode[which(dat.loct.SNPP.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2]),
+                                     sum(dat.loct.SLR.episode[which(dat.loct.SLR.episode[,1] <= 226),2])/sum(dat.loct.episode[1:226,2])
+)
 dat.location.episode.after <- rbind(sum(dat.loct.SH.episode[which(dat.loct.SH.episode[,1] > 226),2])/sum(dat.loct.episode[227:564,2]),
                                     sum(dat.loct.SES.episode[which(dat.loct.SES.episode[,1] > 226),2])/sum(dat.loct.episode[227:564,2]),
                                     sum(dat.loct.MT.episode[which(dat.loct.MT.episode[,1] > 226),2])/sum(dat.loct.episode[227:564,2]),
                                     sum(dat.loct.SNPP.episode[which(dat.loct.SNPP.episode[,1] > 226),2])/sum(dat.loct.episode[227:564,2]),
                                     sum(dat.loct.SLR.episode[which(dat.loct.SLR.episode[,1] > 226),2])/sum(dat.loct.episode[227:564,2])
-                              )
+)
 
 dat.location.episode <- rbind(dat.location.episode.before,dat.location.episode.after)
 dat.location.episode <- cbind(dat.location.episode,c(rep("Before season 10",5), rep("After season 10",5)),
